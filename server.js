87b2be34321bd6d770c9afaf45a80241d01408c1
Bhargav Pattanayak <122164885+Bhargav00007@users.noreply.github.com>
2024-04-21 00:30:34 +0530

    Update server.js
    updated port which was fricked by my friend

--- server.js
+++ server.js
@@ -177,6 +177,7 @@ app.post("/pass", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running at http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
